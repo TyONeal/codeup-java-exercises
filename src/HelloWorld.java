0ab152d0cb03bf737cfb565aa7acc9ea57991d8f
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-07-30 00:47:58 -0500

    Syntax, Types, and Variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,59 @@ public class HelloWorld {
         System.out.println("Hello, world!");
         System.out.print("Hello,");
         System.out.print(" world!");
+
+        int myFavoriteNumber = 19;
+        System.out.println(myFavoriteNumber);
+
+        String mystring = "This is my string.";
+        System.out.println(mystring);
+
+//        mystring = 'a';
+//        mystring = 3.14159;
+
+        long myNumber = 123L;
+//        myNumber = 123;
+
+        // will not compile a double as a long, because of the decimal places.
+
+//        System.out.println(myNumber);
+
+        float myFloat = myNumber;
+
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        //On the second one, changing the value of x with ++x changes its value permanently to 6.
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+//        int newInt = 443234234212341234123413242134123412342341234445;
+//        int newNewInt = newInt + 44;
+//        System.out.println(newNewInt);
+
+        //The int I gave a value was too large to compile. It simply won't run due to conversion.
+
     }
 }
