ba26696e80fc5985710107e6b8072a04905ae3a5
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-07-31 09:53:52 -0500

    Finished ConsoleExercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class ConsoleExercises {
@@ -11,15 +12,56 @@ public class ConsoleExercises {
         int userInput = scanner.nextInt();
         System.out.println("You entered: ---> " + userInput + " <---");
 
-        Scanner scanner2 = new Scanner(System.in);
-        System.out.println("Enter the first word: ");
-        String firstInput = scanner.next();
-        System.out.println("Enter your second word: ");
-        String secondInput = scanner.next();
-        System.out.println("Enter your last word: ");
-        String lastInput = scanner.next();
+//        Scanner scanner2 = new Scanner(System.in);
+//        System.out.println("Enter the first word: ");
+//        String firstInput = scanner.next();
+//        System.out.println("Enter your second word: ");
+//        String secondInput = scanner.next();
+//        System.out.println("Enter your last word: ");
+//        String lastInput = scanner.next();
+//        System.out.println("Your first word was: " + firstInput + ". Then you said: " + secondInput + ". And finally: " + lastInput);
 
-        System.out.println("Your first word was: " + firstInput + ". Then you said: " + secondInput + ". And finally: " + lastInput);
+
+        System.out.println("Enter three words: ");
+        String firstInput = scanner.next();
+        String secondInput = scanner.next();
+        String thirdInput = scanner.next();
+        System.out.println("The first word is: " + firstInput);
+        System.out.println("The second word is: " + secondInput);
+        System.out.println("The third word is: " + thirdInput);
+
+        scanner.nextLine();
+        System.out.println("Please enter a sentence: ");
+        String newSentence = scanner.nextLine();
+        System.out.println("The new sentence is: " + newSentence);
+
+        scanner.useDelimiter("\n");
+
+        scanner.nextLine();
+        System.out.println("Let's calculate the area of your classroom: ");
+        System.out.println("What is the length of your room?: ");
+        int lengthResponse = scanner.nextInt();
+//        float length = (Float.parseFloat(lengthResponse));
+        System.out.println("What is the width of your room?: ");
+        int widthResponse = scanner.nextInt();
+//        float width = (Float.parseFloat(widthResponse));
+
+        System.out.println(lengthResponse * widthResponse);
+        scanner.nextLine();
+        System.out.println("Now let's calculate the perimeter of your classroom: ");
+        System.out.println((2 * lengthResponse) + (2 * widthResponse));
+
+        scanner.nextLine();
+        System.out.println("Finally, let's calculate the volume of your classroom: ");
+        System.out.println("What is the height of your room?: ");
+        int heightResponse = scanner.nextInt();
+//        float height = (Float.parseFloat(heightResponse));
+        System.out.println(widthResponse * lengthResponse * heightResponse);
+
+
+
+
+
 
 
     }
